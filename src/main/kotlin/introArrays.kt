194efa9add392ac70fe67169e11fbba3db444c39
Jose <marinhomoraisster@gmail.com>
2021-01-25 15:36:58 -0300

    kotlin - Arrays 01 - Ranges

--- src/main/kotlin/introArrays.kt
+++ src/main/kotlin/introArrays.kt
@@ -1,25 +1,33 @@
 fun main() {
 
-    val salarios: DoubleArray = doubleArrayOf(1500.50, 2300.0, 5000.0, 8000.0, 10000.0)
-
-    val aumento = 1.1
-    //    for (salario in salarios) {
-//        salarios[indice] = salario * aumento
-//        indice++
-//    }
-
-//    for ((indice, salario) in salarios.withIndex()) {
- //        salarios[indice] = salario * aumento
-//    }
-
-    for (indice in salarios.indices) {
-        salarios[indice] = salarios[indice] * aumento
+    val serie: IntRange = 1.rangeTo(10)
+    for (s in serie) {
+        print("$s ")
     }
-    println(salarios.contentToString())
 
-    salarios.forEachIndexed{
-        i, salario ->
-        salarios[i] = salario * aumento
+    println()
+
+//    val numerosPares = 0..100 step 2
+    val numerosPares = 2.until(100) step 2
+
+    for (numeroPar in numerosPares) {
+        print("$numeroPar ")
     }
-    println(salarios.contentToString())
+
+    println()
+    val numerosParesReverso = 100 downTo 0 step 2
+    numerosParesReverso.forEach { print("$it ") }
+
+    println()
+    val intervalo = 1500.0..5000.0
+    val salario = 5001.0
+    if (salario in intervalo) {
+        println("Está dentro do intervalo")
+    } else {
+        println("Não está dentro do intervalo")
+    }
+
+    val alfabeto = 'a'..'z'
+    val letra = '1'
+    println(letra in alfabeto)
 }
