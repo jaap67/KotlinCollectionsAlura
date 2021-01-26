8ea7caa1512e2d30b3a792f0f3d42b852cc14d13
Jose <marinhomoraisster@gmail.com>
2021-01-26 15:50:55 -0300

    kotlin - Array 01 - Gasto total com Fold

--- src/main/kotlin/introArrays.kt
+++ src/main/kotlin/introArrays.kt
@@ -19,6 +19,12 @@ fun main() {
     val gastoInicial = salariosComAumento.somatoria()
     println(gastoInicial)
 
+    val meses = 6.toBigDecimal()
+    val gastoTotal = salariosComAumento.fold(gastoInicial) { acumulador, salarioAumentado ->
+        acumulador + (salarioAumentado * meses).setScale(2, RoundingMode.UP)
+    }
+
+    println(gastoTotal)
 }
 
 private fun calculaAumentoRelativo(salario: BigDecimal, aumento: BigDecimal) =
