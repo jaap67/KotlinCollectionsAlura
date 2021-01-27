86d818c34a5a703088a4869eb4e2e34875abbb07
Jose <marinhomoraisster@gmail.com>
2021-01-27 18:22:22 -0300

    kotlin - 01 - Ordenando listas

--- src/main/kotlin/list/Livro.kt
+++ src/main/kotlin/list/Livro.kt
@@ -3,6 +3,10 @@ package list
 data class Livro(
     val titulo: String,
     val autor: String,
-    val anoPublicacao: Int,
+    val anoPublicacao: Long,
     val editora: String? = null
-)
+) : Comparable<Livro> {
+    override fun compareTo(other: Livro): Int {
+        return this.anoPublicacao.compareTo(other.anoPublicacao)
+    }
+}
--- src/main/kotlin/list/listaDeLivros.kt
+++ src/main/kotlin/list/listaDeLivros.kt
@@ -44,9 +44,17 @@ fun main() {
 //    println(livros)
 
     livros.imprimeComMarcadores()
+
+    val ordenadoAnoPublicacao = livros.sorted()
+    ordenadoAnoPublicacao.imprimeComMarcadores()
+
+    val ordenadoPorTitulo = livros.sortedBy { it.titulo }
+    ordenadoPorTitulo.imprimeComMarcadores()
+
+    livros.sortedBy { it.autor }.imprimeComMarcadores()
 }
 
-fun MutableList<Livro>.imprimeComMarcadores(){
+fun List<Livro>.imprimeComMarcadores(){
     val textoFormatado = this.joinToString(separator = "\n") {
         " - ${it.titulo} de ${it.autor}"
     }
