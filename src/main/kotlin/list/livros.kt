fd10c4add84eb5d9653da86b2287f85877e5404a
Jose <marinhomoraisster@gmail.com>
2021-01-28 11:51:36 -0300

    kotlin - Array 01 - Lidando com nulos

--- src/main/kotlin/list/livros.kt
+++ src/main/kotlin/list/livros.kt
@@ -1,6 +1,6 @@
 package list
 
-val listaDeLivros = mutableListOf(
+val listaDeLivros: MutableList<Livro> = mutableListOf(
     Livro(
         titulo = "Grande Sertão: Vereda",
         autor = "João Guimarães Rosa",
@@ -16,6 +16,56 @@ val listaDeLivros = mutableListOf(
         autor = "Machado de Assis",
         anoPublicacao = 1881
     ),
+    Livro(
+        titulo = "Saganara",
+        autor = "João Guimarães Rosa",
+        anoPublicacao = 1946
+    ),
+    Livro(
+        titulo = "Iracema",
+        autor = "José de Alencar",
+        anoPublicacao = 1865,
+        editora = "Editora B"
+    ),
+    Livro(
+        titulo = "Vidas Secas",
+        autor = "Graciliano Ramos",
+        anoPublicacao = 1938,
+        editora = "Editora A"
+    ),
+    Livro(
+        titulo = "Mayombe",
+        autor = "Pepela",
+        anoPublicacao = 1979,
+        editora = "Editora B"
+    ),
+    Livro(
+        titulo = "O Cortiço",
+        autor = "Aluísio Azevedo",
+        anoPublicacao = 1890,
+        editora = "Editora B"
+    )
+)
+
+val listaLivrosComNulos: MutableList<Livro?> = mutableListOf(
+    null,
+    Livro(
+        titulo = "Grande Sertão: Vereda",
+        autor = "João Guimarães Rosa",
+        anoPublicacao = 1956
+    ),
+    null,
+    Livro(
+        titulo = "Minha vida de menina",
+        autor = "Helena Morley",
+        anoPublicacao = 1942
+    ),
+    Livro(
+        titulo = "Memórias Póstumas de Brás Cubas",
+        autor = "Machado de Assis",
+        anoPublicacao = 1881
+    ),
+    null,
     Livro(
         titulo = "Saganara",
         autor = "João Guimarães Rosa",
