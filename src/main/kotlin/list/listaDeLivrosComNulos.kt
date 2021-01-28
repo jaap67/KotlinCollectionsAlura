fa92d5be38d5f5b819efc0c935bea0ccefa8b6a1
Jose <marinhomoraisster@gmail.com>
2021-01-28 14:39:08 -0300

    kotlin - Array 01 - Lidando com atributos nulos

--- src/main/kotlin/list/listaDeLivrosComNulos.kt
+++ src/main/kotlin/list/listaDeLivrosComNulos.kt
@@ -1,5 +1,10 @@
 package list
 
 fun main() {
-    listaLivrosComNulos.imprimeComMarcadores()
+//    listaLivrosComNulos.imprimeComMarcadores()
+    listaDeLivros
+        .groupBy { it.editora ?: "Editora desconhecida"}
+        .forEach { (editora: String?, livros: List<Livro>) ->
+            println("$editora: ${livros.joinToString { it.titulo }}")
+        }
 }
